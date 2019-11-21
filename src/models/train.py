101dcfa299cf01f20d5ca08be3a7ebef2b93e30b
Viliam Schrojf <viliam@schrojf.sk>
2019-11-21 12:56:22 +0100

    Fix import modelus

---
+++ src/models/train.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('./')
+
+from src.data import WIKISequence
+
+# load data as df
+# initialize train and test Sequence
+# test = WIKISequence(...)
+# train model
+
+print("ok")
