15ddc78f0a6245b3c876e2e719ff7e2c860e9f3e
Viliam Schrojf <viliam@schrojf.sk>
2019-12-01 17:39:06 +0100

    Added builder script for VGG Face model graphs

--- src/models/VGGFaceModel.py
+++ src/models/VGGFaceModel.py
@@ -55,4 +55,36 @@ def create_vgg_face_model():
 def load_pretrained_weights(model):
     model.load_weights('../data/vgg_face_weights.h5')
 
-# todo
+
+def build_new_class_model(classes, learning_rate=0.001, loss='sparse_categorical_crossentropy', metrics='accuracy',
+                          lock_layers=True):
+    vgg_face_model = create_vgg_face_model()
+    load_pretrained_weights(vgg_face_model)
+
+    if lock_layers:
+        for layer in vgg_face_model.layers[:-7]:
+            layer.trainable = False
+
+    # Replace last 4 layers with classes and softmax
+    face_model_output = keras.layers.Convolution2D(classes, (1, 1), name='predictions')(vgg_face_model.layers[-4].output)
+    face_model_output = keras.layers.Flatten()(face_model_output)
+    face_model_output = keras.layers.Activation('softmax')(face_model_output)
+
+    age_model = keras.Model(inputs=vgg_face_model.input, outputs=face_model_output)
+
+    age_model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+        loss=loss,
+        metrics=[metrics])
+
+    return age_model
+
+
+def build_new_age_model(learning_rate=0.001, loss='sparse_categorical_crossentropy', metrics='accuracy',
+                        lock_layers=True):
+    return build_new_class_model(101, learning_rate, loss, metrics, lock_layers)
+
+
+def build_new_gender_model(learning_rate=0.001, loss='sparse_categorical_crossentropy', metrics='accuracy',
+                           lock_layers=True):
+    return build_new_class_model(2, learning_rate, loss, metrics, lock_layers)
